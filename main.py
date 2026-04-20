bd535924ef4780a4d58065a3589c6f3f499b04e9
IIChibi0ChanII <mahamudashanta@gmail.com>
2026-04-20 18:29:36 +0600

    f

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def max_sum(arr):
+def max_product(arr):
     if arr is None or len(arr) < 2:
         return None
 
     max1 = max2 = float('-inf')
+    min1 = min2 = float('inf')
 
     for num in arr:
         if num > max1:
@@ -11,8 +12,14 @@ def max_sum(arr):
         elif num > max2:
             max2 = num
 
-    return max1 + max2
+        if num < min1:
+            min2 = min1
+            min1 = num
+        elif num < min2:
+            min2 = num
+
+    return max(max1 * max2, min1 * min2)
 
 
 arr = [1, 2, 6, 4, 5, 3, 7]
-print(max_sum(arr))
+print(max_product(arr))
